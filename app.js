bb79e8ab9bc2940e479120221316fecb3313dd44
matthewcho1211 <matthewcho1234@gmail.com>
2023-06-19 17:18:48 +0800

    add basic style

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+
 const Food = require("./models/food");
 
 const app = express();
+app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
